c77f9be4cffa7c8815dc56c56c434836666f0108
Alexander Senier <senier@componolit.com>
2019-03-21 19:43:13 +0100

    Use random radius when importing graph
    Ref. #9

--- src/scsc-random.adb
+++ src/scsc-random.adb
@@ -3,12 +3,10 @@ with Ada.Numerics.Discrete_Random;
 package body SCSC.Random
    with SPARK_Mode => Off
 is
-
-   package DR is new Ada.Numerics.Discrete_Random (Natural);
-
+   package DR is new Ada.Numerics.Discrete_Random (Result_Type);
    G : DR.Generator;
 
-   function Get_Random return Natural
+   function Get_Random return Result_Type
    is
    begin
       return DR.Random (G);
